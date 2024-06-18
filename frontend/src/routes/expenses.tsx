a97a225540efe1c8bbc2384386a08aa129e52a66
Dmitry Drigota <drigota@gmail.com>
2024-06-18 16:29:18 +0100

    add expenses and create-expense routes

---
+++ frontend/src/routes/expenses.tsx
@@ -0,0 +1,8 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/expenses")({
+  component: Expenses,
+});
+function Expenses() {
+  return <div className="p-2">Show All Expenses</div>;
+}
