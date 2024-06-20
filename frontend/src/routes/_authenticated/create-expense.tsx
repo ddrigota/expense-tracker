ee7c24ba7d9894c238f0d61a14cd9fd877bc7a61
Dmitry Drigota <drigota@gmail.com>
2024-06-20 12:38:24 +0100

    setup postgres db via drizzle and neon

--- frontend/src/routes/_authenticated/create-expense.tsx
+++ frontend/src/routes/_authenticated/create-expense.tsx
@@ -14,7 +14,7 @@ function CreateExpense() {
   const form = useForm({
     defaultValues: {
       title: "",
-      amount: 0,
+      amount: "",
     },
     onSubmit: async ({ value }) => {
       const res = await api.expenses.$post({ json: value });
@@ -68,7 +68,7 @@ function CreateExpense() {
                   type="number"
                   value={field.state.value}
                   onBlur={field.handleBlur}
-                  onChange={(e) => field.handleChange(Number(e.target.value))}
+                  onChange={(e) => field.handleChange(e.target.value)}
                 />
                 {field.state.meta.touchedErrors ? (
                   <em>{field.state.meta.touchedErrors}</em>
--- server/routes/expenses.ts
+++ server/routes/expenses.ts
@@ -1,38 +1,55 @@
 import { zValidator } from "@hono/zod-validator";
+import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 import { z } from "zod";
+import { db } from "../db";
+import { expenses as expensesTable } from "../db/schema/expenses";
 import { getUser } from "../kinde";
 
 const expenseSchema = z.object({
   id: z.number().int().positive().min(1),
   title: z.string().min(3).max(100),
-  amount: z.number().int().positive(),
+  amount: z.string(),
 });
 
 type Expense = z.infer<typeof expenseSchema>;
 const createPostSchema = expenseSchema.omit({ id: true });
 
 const fakeExpenses: Expense[] = [
-  { id: 1, title: "Rent", amount: 1000 },
-  { id: 2, title: "Food", amount: 200 },
-  { id: 3, title: "Transport", amount: 100 },
-  { id: 4, title: "Entertainment", amount: 50 },
-  { id: 5, title: "Utilities", amount: 150 },
+  { id: 1, title: "Rent", amount: "1000" },
+  { id: 2, title: "Food", amount: "200" },
+  { id: 3, title: "Transport", amount: "100" },
+  { id: 4, title: "Entertainment", amount: "50" },
+  { id: 5, title: "Utilities", amount: "150" },
 ];
 
 export const expensesRoute = new Hono()
   .get("/", getUser, async c => {
     const user = c.var.user;
-    return c.json({ expenses: fakeExpenses });
+    const expenses = await db
+      .select()
+      .from(expensesTable)
+      .where(eq(expensesTable.userId, user.id));
+    return c.json({ expenses: expenses });
   })
   .post("/", getUser, zValidator("json", createPostSchema), async c => {
     const expense = await c.req.valid("json");
-    fakeExpenses.push({ id: fakeExpenses.length + 1, ...expense });
+    const user = c.var.user;
+    const result = await db
+      .insert(expensesTable)
+      .values({
+        ...expense,
+        userId: user.id,
+      })
+      .returning();
     c.status(201);
-    return c.json({ expense });
+    return c.json(result);
   })
   .get("/total-spent", getUser, c => {
-    const total = fakeExpenses.reduce((acc, expense) => acc + expense.amount, 0);
+    const total = fakeExpenses.reduce(
+      (acc, expense) => acc + +expense.amount,
+      0
+    );
     return c.json({ total });
   })
   .get("/:id{[0-9]+}", getUser, c => {
